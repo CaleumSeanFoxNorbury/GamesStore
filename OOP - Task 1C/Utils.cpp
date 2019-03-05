43904189f785a4ab63fdf402a4a5834371e02b11
Fault <37138291+Adrian-CH@users.noreply.github.com>
2019-03-05 12:13:02 +0000

    Create Date Format
    Start on creating date format, still needs a little to make it fully work.

--- OOP - Task 1C/Utils.cpp
+++ OOP - Task 1C/Utils.cpp
@@ -11,3 +11,14 @@ char Utils::GetCharFromUser()//ERROR HERE, TAKES FIRST CHAR
 {
 	return toupper(GetLineFromUser()[0]);
 }
+
+const std::string Utils::currentDate() //returns const current date as buf, buf is std::string
+{
+	//time_t now = time(0);
+	//struct tm tstruct;
+	//char buf[80];
+	//tstruct = *localtime(&now);
+	//strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
+	//return buf;
+	//this won't work currently, localtime is depreciated. localtime_s, wants two args but we don't want it.
+}
--- OOP - Task 1C/Utils.h
+++ OOP - Task 1C/Utils.h
@@ -2,10 +2,14 @@
 
 #include <string>
 #include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <ctime>
 
 class Utils
 {
   public:
 	static std::string GetLineFromUser();
 	static char GetCharFromUser();
+	const static std::string currentDate();
 };
